training bug fixed. But there exist gradient vanishing problem

models/deep_mesh_flow.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
 import torch
 import argparse
-from .model_parts import *
 
 import sys
 sys.path.append("..")
+
+from models.model_parts import feature_extractor, mask_predictor, mesh_estimator_body, mesh_estimator_head, mesh_selector
 from utils.solve_DLT import solve_mesh_flow_DLT, spatial_transform_by_grid
 
 class DeepMeshFlow(nn.Module):
@@ -18,10 +19,10 @@ class DeepMeshFlow(nn.Module):
         self.feature_extractor = feature_extractor(in_channels=1, out_channels=1)
         self.mask_predictor = mask_predictor(in_channels=1)
         self.estimator_body = mesh_estimator_body
-        self.estimator_head_0 = mesh_estimator_head(args.mesh_size_1, args.mesh_size_3, self.img_size)
-        self.estimator_head_1 = mesh_estimator_head(args.mesh_size_2, args.mesh_size_3, self.img_size)
-        self.estimator_head_2 = mesh_estimator_head(args.mesh_size_3, args.mesh_size_3, self.img_size)
-        self.mesh_selector = mesh_selector(2, (3, 17, 17))
+        self.estimator_head_0 = mesh_estimator_head(args.mesh_size_1, args.mesh_size_3, self.img_size, device=self.device)
+        self.estimator_head_1 = mesh_estimator_head(args.mesh_size_2, args.mesh_size_3, self.img_size, device=self.device)
+        self.estimator_head_2 = mesh_estimator_head(args.mesh_size_3, args.mesh_size_3, self.img_size, device=self.device)
+        self.mesh_selector = mesh_selector(in_channels=2, out_shape=(3, 2, 17, 17))
     
     def forward(self, x):
         input1 = x[:, 0:3, :, :].mean(dim=1, keepdim=True)
@@ -35,21 +36,21 @@ class DeepMeshFlow(nn.Module):
         feature_processed = torch.cat([feature1 * mask1, feature2 * mask2], dim=1)
         out = self.estimator_body(feature_processed)
 
-        mesh_flow_0 = self.estimator_head_0(out)
-        mesh_flow_1 = self.estimator_head_1(out)
-        mesh_flow_2 = self.estimator_head_2(out)
+        mesh_flow_0 = self.estimator_head_0(out).unsqueeze(1)
+        mesh_flow_1 = self.estimator_head_1(out).unsqueeze(1)
+        mesh_flow_2 = self.estimator_head_2(out).unsqueeze(1)
         mesh_flow = torch.cat([mesh_flow_0, mesh_flow_1, mesh_flow_2], dim=1)
         mesh_index = torch.argmax(self.mesh_selector(torch.cat([input1, input2], dim=1)), dim=1, keepdim=True)
-        mesh_out = torch.gather(mesh_flow, dim=1, index=mesh_index)
+        mesh_out = torch.gather(mesh_flow, dim=1, index=mesh_index).squeeze(1)
 
         feature_processed_inv = torch.cat([feature2 * mask2, feature1 * mask1], dim=1)
         out_inv = self.estimator_body(feature_processed_inv)
-        mesh_flow_0_inv = self.estimator_head_0(out_inv)
-        mesh_flow_1_inv = self.estimator_head_1(out_inv)
-        mesh_flow_2_inv = self.estimator_head_2(out_inv)
+        mesh_flow_0_inv = self.estimator_head_0(out_inv).unsqueeze(1)
+        mesh_flow_1_inv = self.estimator_head_1(out_inv).unsqueeze(1)
+        mesh_flow_2_inv = self.estimator_head_2(out_inv).unsqueeze(1)
         mesh_flow_inv = torch.cat([mesh_flow_0_inv, mesh_flow_1_inv, mesh_flow_2_inv], dim=1)
         mesh_index_inv = torch.argmax(self.mesh_selector(torch.cat([input2, input1], dim=1)), dim=1, keepdim=True)
-        mesh_out_inv = torch.gather(mesh_flow_inv, dim=1, index=mesh_index_inv)
+        mesh_out_inv = torch.gather(mesh_flow_inv, dim=1, index=mesh_index_inv).squeeze(1)
         
         # use mesh warp original images
         warped_grid, homography_grid = solve_mesh_flow_DLT(mesh_flow=mesh_out, device=self.device, image_size=self.img_size, patch_size=self.patch_size)
@@ -70,5 +71,7 @@ class DeepMeshFlow(nn.Module):
 
 if __name__ == "__main__":
     from torchsummary import summary
-    model = DeepMeshFlow()
-    print(summary(model, (2, 128, 128), device='cpu'))
+    from config.option import args
+    device = torch.device("cuda:0")
+    model = DeepMeshFlow(args, device=torch.device("cpu"))
+    print(summary(model, (12, 128, 128), device='cpu'))

models/mesh_flow_upsample.py
@@ -5,7 +5,7 @@ from typing import *
 from torchgeometry.core.transformations import transform_points
 
 
-def mesh_flow_upsampling(mesh_flow_tensor:torch.Tensor, mesh_grid_size:Tuple[int], upsample_grid_size:Tuple[int], img_size:Tuple[int], batch_size:int):
+def mesh_flow_upsampling(mesh_flow_tensor:torch.Tensor, mesh_grid_size:Tuple[int], upsample_grid_size:Tuple[int], img_size:Tuple[int], batch_size:int, device:torch.device):
     """upsample mesh flow result from mesh grid size to upsampling grid size
 
     Args:
@@ -14,6 +14,7 @@ def mesh_flow_upsampling(mesh_flow_tensor:torch.Tensor, mesh_grid_size:Tuple[int
       upsample_grid_size: mesh grid of high resolutin
       img_size: size of input image
       batch_size: batch size of data
+      device: torch device (cpu or gpu)
     
     Returns:
       upsampled mesh flow tensor with shape: (N, 2, upsample_grid_size[0], upsample_grid_size[1])
@@ -25,8 +26,10 @@ def mesh_flow_upsampling(mesh_flow_tensor:torch.Tensor, mesh_grid_size:Tuple[int
     mesh_flow = mesh_flow.reshape(mesh_flow.shape[0] * mesh_flow.shape[1], -1)
     mesh_flow = mesh_flow.reshape(-1, 2, 4).permute(0, 2, 1)
     # step 2: get mesh grid of mesh_grid size
-    y_t = torch.matmul(torch.ones(mesh_grid_size[0], 1), torch.linspace(0, img_size[1], mesh_grid_size[1]).unsqueeze(0)).unsqueeze(2)
-    x_t = torch.matmul(torch.linspace(0, img_size[0], mesh_grid_size[0]).unsqueeze(1), torch.ones(1, mesh_grid_size[1])).unsqueeze(2)
+    y_t = torch.matmul(torch.ones(mesh_grid_size[0], 1, dtype=torch.float, device=device), \
+      torch.linspace(0, img_size[1], mesh_grid_size[1], dtype=torch.float, device=device).unsqueeze(0)).unsqueeze(2)
+    x_t = torch.matmul(torch.linspace(0, img_size[0], mesh_grid_size[0], dtype=torch.float, device=device).unsqueeze(1), \
+      torch.ones(1, mesh_grid_size[1], dtype=torch.float, device=device)).unsqueeze(2)
     # grid_sparse shape: [batch_size, 2, mesh_grid_size[0], mesh_grid_size[1]]
     grid_sparse = torch.cat([x_t, y_t], dim=2).permute(2, 0, 1).unsqueeze(0).expand(batch_size, -1, -1, -1)
     # step 3: unfold sparse grid
@@ -45,8 +48,10 @@ def mesh_flow_upsampling(mesh_flow_tensor:torch.Tensor, mesh_grid_size:Tuple[int
     # solved_matrices_scaled = H_scale(solved_matrices, patch_width=patch_width, patch_height=patch_height, batch_size=batch_size)
 
     # step 5: get dense grid of upsample_grid_size
-    y_t = torch.matmul(torch.ones(upsample_grid_size[0], 1), torch.linspace(0, img_size[1], upsample_grid_size[1]).unsqueeze(0)).unsqueeze(2)
-    x_t = torch.matmul(torch.linspace(0, img_size[0], upsample_grid_size[0]).unsqueeze(1), torch.ones(1, upsample_grid_size[1])).unsqueeze(2)
+    y_t = torch.matmul(torch.ones(upsample_grid_size[0], 1, dtype=torch.float, device=device), \
+      torch.linspace(0, img_size[1], upsample_grid_size[1], dtype=torch.float, device=device).unsqueeze(0)).unsqueeze(2)
+    x_t = torch.matmul(torch.linspace(0, img_size[0], upsample_grid_size[0], dtype=torch.float, device=device).unsqueeze(1), \
+      torch.ones(1, upsample_grid_size[1], dtype=torch.float, device=device)).unsqueeze(2)
     # grid shape: [batch_size, 2, upsample_grid_size[0], upsample_grid_size[1]]
     grid_dense = torch.cat([x_t, y_t], dim=2).permute(2, 0, 1).unsqueeze(0).expand(batch_size, -1, -1, -1)
     # step 6: unfold dense grid

models/model_parts.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 from typing import *
 
 from .mesh_flow_upsample import mesh_flow_upsampling
@@ -56,27 +57,28 @@ class mask_predictor(nn.Module):
         return out
 
 class mesh_selector(nn.Module):
-    def __init__(self, in_channels=2, out_shape=(3, 17, 17)) -> None:
+    def __init__(self, in_channels=2, out_shape=(3, 2, 17, 17)) -> None:
         super().__init__()
         self.out_shape = out_shape
-        self.out_feature_num = out_shape[0] * out_shape[1] * out_shape[2]
+        self.out_feature_num = out_shape[0] * out_shape[1] * out_shape[2] * out_shape[3]
         self.features = ResNet(BasicBlock, [2, 2, 2, 2], in_channels)
         self.fc = nn.Linear(512, self.out_feature_num)
     
     def forward(self, x):
         out = self.features(x)
         out = self.fc(out)
-        out = out.view(-1, self.out_shape[0], self.out_shape[1], self.out_shape[2])
+        out = out.view(-1, self.out_shape[0], self.out_shape[1], self.out_shape[2], self.out_shape[3])
         return out
 
 mesh_estimator_body = resnet34(in_channels=2)
 
 class mesh_estimator_head(nn.Module):
-    def __init__(self, mesh_grid_size:Tuple[int], upsample_size:Tuple[int], image_size:Tuple[int]) -> None:
+    def __init__(self, mesh_grid_size:Tuple[int], upsample_size:Tuple[int], image_size:Tuple[int], device:torch.device) -> None:
         super().__init__()
         self.mesh_grid_size = mesh_grid_size
         self.upsample_size = upsample_size
         self.image_size = image_size
+        self.device = device
         fc_out_size = mesh_grid_size[0] * mesh_grid_size[1] * 2
         self.fc1 = nn.Sequential(
             nn.Linear(512, 1024),
@@ -95,9 +97,9 @@ class mesh_estimator_head(nn.Module):
         out = self.fc1(x)
         # out = self.fc2(out)
         out = self.fc3(out)
-        out = out.view(out.shape[0], 2, self.mesh_grid_size[0], self.mesh_grid_size[1])
+        out = out.view(x.shape[0], 2, self.mesh_grid_size[0], self.mesh_grid_size[1])
         if self.upsample_size != self.mesh_grid_size:
-            out = mesh_flow_upsampling(out, self.mesh_grid_size, self.upsample_size, self.image_size, x.shape[0])
+            out = mesh_flow_upsampling(out, self.mesh_grid_size, self.upsample_size, self.image_size, x.shape[0], device=self.device)
         
         return out
 

train.py
@@ -72,7 +72,7 @@ def train(args):
             # TODO: Does L ab loss should be normalized ?
             L_ab = -1 * torch.abs(feature1_orig - feature2_orig).mean()
             Identity = torch.eye(3, dtype=torch.float, device=device).unsqueeze(0).expand(homography_grid.shape[0], -1, -1)
-            L_inverse = torch.sum(torch.abs(torch.matmul(homography_grid, homography_grid_inv) - Identity))
+            L_inverse = torch.mean(torch.abs(torch.matmul(homography_grid, homography_grid_inv) - Identity))
             loss = ln + ln_inv + args.loss_weight_lambda * L_ab + args.loss_weight_mu * L_inverse
 
             optimizer.zero_grad()

utils/solve_DLT.py
@@ -7,32 +7,30 @@ import torch
 def solve_mesh_flow_DLT(mesh_flow: torch.Tensor, device: torch.device, patch_size: Tuple[int], image_size: Tuple[int]) -> Tuple[torch.Tensor]:
     batch_size = mesh_flow.shape[0]
     unfold_mesh_flow = grid2pointgroup(mesh_flow, (2, 2))
-    mesh_grid_X, mesh_grid_Y = torch.meshgrid(torch.arange(start=0, end=image_size[0] + 1, step=patch_size[0], dtype=torch.long, device=device),
-        torch.arange(start=0, end=image_size[1] + 1, step=patch_size[1], dtype=torch.long, device=device,),
+    mesh_grid_X, mesh_grid_Y = torch.meshgrid(torch.arange(start=0, end=image_size[0] + 1, step=patch_size[0], dtype=torch.float, device=device),
+        torch.arange(start=0, end=image_size[1] + 1, step=patch_size[1], dtype=torch.float, device=device,),
         indexing="ij",
     )
-    mesh_grid = torch.cat([mesh_grid_X.unsqueeze(0), mesh_grid_Y.unsqueeze(0)], dim=0).unsqueeze(0).expand(batch_size, 1, 1, 1)
+    mesh_grid = torch.cat([mesh_grid_X.unsqueeze(0), mesh_grid_Y.unsqueeze(0)], dim=0).unsqueeze(0).expand(batch_size, -1, -1, -1)
     unfold_mesh_grid = grid2pointgroup(mesh_grid, (2, 2))
     solved_matrices = torchgeometry.get_perspective_transform(unfold_mesh_grid, unfold_mesh_flow + unfold_mesh_grid)
-    solved_matrices = H_scale(solved_matrices, patch_size[0], patch_size[1], batch_size)
+    solved_matrices = H_scale(solved_matrices, patch_size[0], patch_size[1], unfold_mesh_grid.shape[0], device=device)
 
     # warp image grid points
-    points_grid = torch.meshgrid(torch.arange(image_size[0]), torch.arange(image_size[1]), indexing='ij')
-    points_grid = torch.cat([points_grid[0].unsqueeze(0), points_grid[1].unsqueeze(0)], dim=0).unsqueeze(0).expand(batch_size, 1, 1, 1)
-    grid_unfolder = nn.Unfold(kernel_size=(image_size[0] // patch_size[0], image_size[1] // patch_size[1]), 
-        stride=(image_size[0] // patch_size[0], image_size[1] // patch_size[1])
-    )
+    points_grid = torch.meshgrid(torch.arange(image_size[0], dtype=torch.float, device=device), \
+        torch.arange(image_size[1], dtype=torch.float, device=device), indexing='ij')
+    points_grid = torch.cat([points_grid[0].unsqueeze(0), points_grid[1].unsqueeze(0)], dim=0).unsqueeze(0).expand(batch_size, -1, -1, -1)
+    grid_unfolder = nn.Unfold(kernel_size=(patch_size[0], patch_size[1]), stride=(patch_size[0], patch_size[1]))
     # unfold_points_grid shape: [N, 2 * patch_size * patch_size, grid_size * grid_size]
     unfold_points_grid = grid_unfolder(points_grid)
     # after reshape and permutation: [N * grid_size * grid_size, patch_size * patch_size, 2]
-    unfold_points_grid = unfold_points_grid.permute(0, 2, 1).reshape(-1, 2 * patch_size * patch_size).reshape(-1, 2, patch_size * patch_size).permute(0, 2, 1)
-    warped_unfold_points_grid = torchgeometry.core.transform_points(solved_matrices)
+    unfold_points_grid = unfold_points_grid.permute(0, 2, 1).reshape(-1, 2 * patch_size[0] * patch_size[1]).reshape(-1, 2, patch_size[0] * patch_size[1]).permute(0, 2, 1)
+    warped_unfold_points_grid = torchgeometry.core.transform_points(solved_matrices, unfold_points_grid)
 
     # FOLD BACK
-    warped_unfold_points_grid = warped_unfold_points_grid.permute(0, 2, 1).reshape(-1, 2 * patch_size * patch_size).reshape(batch_size, -1, 2 * patch_size * patch_size) \
-        .permute(0, 2, 1)
-    folder = nn.Fold(output_size=image_size, kernel_size=(image_size[0] // patch_size[0], image_size[1] // patch_size[1]), 
-        stride=(image_size[0] // patch_size[0], image_size[1] // patch_size[1]))
+    warped_unfold_points_grid = warped_unfold_points_grid.permute(0, 2, 1).reshape(-1, 2 * patch_size[0] * patch_size[1]). \
+        reshape(batch_size, -1, 2 * patch_size[0] * patch_size[1]).permute(0, 2, 1)
+    folder = nn.Fold(output_size=image_size, kernel_size=(patch_size[0], patch_size[1]), stride=(patch_size[0], patch_size[1]))
     warped_points_grid = folder(warped_unfold_points_grid)
 
     return warped_points_grid, solved_matrices
@@ -43,14 +41,14 @@ def grid2pointgroup(grid:torch.Tensor, kernel_size:Tuple[int]) -> torch.Tensor:
     """
     unfolder = nn.Unfold(kernel_size=kernel_size)
     grid_unfold = unfolder(grid)
-    grid_unfold = grid_unfold.permute(0, 2, 1).reshape(-1, 8).reshape(-1, 2, 4).permute(0, 1, 2)
+    grid_unfold = grid_unfold.permute(0, 2, 1).reshape(-1, 8).reshape(-1, 2, 4).permute(0, 2, 1)
 
     return grid_unfold
 
-def H_scale(H:torch.Tensor, patch_width:float, patch_height:float, batch_size:int) -> torch.Tensor:
+def H_scale(H:torch.Tensor, patch_width:float, patch_height:float, batch_size:int, device:torch.device) -> torch.Tensor:
     M = torch.tensor([[patch_height / 2.0, 0., patch_height / 2.0],
                         [0., patch_width / 2.0, patch_width / 2.0],
-                        [0., 0., 1.]], dtype=torch.float32)
+                        [0., 0., 1.]], dtype=torch.float32, device=device)
     M_inv = torch.linalg.inv(M)
     M_tile = torch.tile(M.unsqueeze(0), dims=[batch_size, 1, 1])
     M_inv_tile = torch.tile(M_inv.unsqueeze(0), dims=[batch_size, 1, 1])
@@ -93,8 +91,6 @@ def spatial_transform_by_grid(img:torch.Tensor, grid:torch.Tensor, device:torch.
     y0_f = y0.float()
     y1_f = y1.float()
     # test
-    print(x0_f[-1], x1_f[-1], y0_f[-1], y1_f[-1])
-    print(x[-1], y[-1])
     
     wa = torch.unsqueeze(((x1_f - x) * (y1_f - y)), 1)
     wb = torch.unsqueeze(((x1_f - x) * (y - y0_f)), 1)
@@ -109,27 +105,13 @@ def spatial_transform_by_grid(img:torch.Tensor, grid:torch.Tensor, device:torch.
     return output 
 
 # Test
-# if __name__ == "__main__":
-#     from PIL import Image
-#     from torchvision import transforms
-#     import cv2
-#     import numpy as np
-#     im = Image.open("/home/wyq/DeepImageStitching-pytorch/align_data/train/input1/000001.jpg")
-#     im.save("raw.jpg")
-#     im_t = (transforms.ToTensor()(im)).unsqueeze(0)
-#     # im_t = F.pad(im_t, (1, 1, 1, 1), 'constant', 0)
-#     points_grid = torch.meshgrid(torch.arange(128, dtype=torch.float), torch.arange(128, dtype=torch.float), indexing='ij')
-#     points_grid = torch.cat([points_grid[0].unsqueeze(0), points_grid[1].unsqueeze(0)], dim=0).unsqueeze(0)
-#     device = torch.device("cpu")
-#     out = spatial_transform_by_grid(im_t, points_grid, device)
-#     out_im = transforms.ToPILImage()(out[0])
-#     out_im.save("test.jpg")
-#     a = cv2.imread("test.jpg")
-#     b = cv2.imread("raw.jpg")
-#     print(((a - b) ** 2).mean())
-    # im = cv2.imread("/home/wyq/DeepImageStitching-pytorch/align_data/train/input1/000001.jpg")
-    # src = np.array([[0, 0], [127, 0], [0, 127], [127, 127]])
-    # homograhy, _ = cv2.findHomography(src, src)
-    # im_out = cv2.warpPerspective(im, homograhy, (128, 128))
-    # print(np.abs(im_out - im).sum())
+if __name__ == "__main__":
+    mesh_flow = torch.randn(8, 2, 17, 17)
+
+    b, c = solve_mesh_flow_DLT(mesh_flow, torch.device("cpu"), (8, 8), (128, 128))
+    print(b.shape, c.shape)
+    # a = torch.arange(16).reshape(2, 2, 2, 2)
+    # b = a + 1
+    # ab = torch.cat([a.unsqueeze(1), b.unsqueeze(1)], dim=1)
+    # c = torch.randn(size=(2, 1, 2, ))
 